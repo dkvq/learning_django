35a2dbaa65618aa6204f60abbdc1e6597913f645
dkvq <diakivasyl@gmail.com>
2023-02-10 16:30:50 +0200

    Added tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,7 @@
 import time
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycong2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
